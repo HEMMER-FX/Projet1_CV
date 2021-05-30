3cd445f42d3d9bc8be9bfec0db6ac9fc192c731b
HEMMER <79737617+HEMMER-FX@users.noreply.github.com>
2021-05-30 21:39:30 +0200

    Add files via upload

---
+++ pages/histoire.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Histoire de Eric HOCHET</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" media="screen" href="../css/style_histoire_mobile.css" type="text/css" />
+    <link rel="stylesheet" media="screen" href="../css/style_histoire_print.css" type="text/css" />
+    <link rel="stylesheet" media="screen" href="../css/global.css" type="text/css" />
+</head>
+
+<body>
+<!-- grand div pour le contenu -->
+<div id="grandContenaire">
+    <nav id="art-nav">   <!-- barre de navigation idem toutes pages -->
+        <ul id="menu-deroulant">
+            <li><a href="#" id="alt-titreHide">Histoire</a> <!-- id pour cacher le tire -->
+                <ul>
+                    <li><a href="../index.php">Accueil</a></li>
+                    <li><a id="titre_navbar" href="compétences.php">Compétences</a></li>
+                    <li><a href="articles.php">Article</a></li>
+                    <li><a href="quizz.php">Quizz</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="section_histoire"> <!-- div pour placer les sections -->
+        <header> <!-- pour placer le tire de la page -->
+            <h1 id="titre">Histoire</h1>
+        </header>
+
+        <main> <!-- Le contenu principal de notre page -->
+
+            <div class="contenu_main">
+                <h2>Naissance</h2>
+                <p> <span class="souligne">C'est le 28 octobre 1984 </span> que j'ai vu le jour. En effet mes parents <span class="important"> Nadine Amo </span> ainsi que mon père <span class="important"> Bale Hochet </span> sont fiers de présenter à toute la famille ma venue, cela faisait trop longtemps que mes parents m'attendaient. <br>
+                    Mes parents sont passés par des épreuves de la vie très compliquées, ma maman était enceinte un peu avant de m'avoir mais malencontreusement cela s'est <span class="important"> mal passé </span> car pour son anniversaire mon père lui a offert un saut en élastique qu'elle s'est empressée de faire et au moment de remonter elle a vu quelque chose
+                    tomber, c'est à ce moment là qu'elle a su qu'elle était <span class="important"> enceinte </span>. Depuis pour sa nouvelle grossesse ma maman est restée à la maison .</p>
+            </div>
+
+            <div class="contenu_main">
+                <h2>Collège</h2>
+                <p> <span class="souligne">A l'âge de 12 ans </span>je suis entré au collège , j'ai toujours été un enfant malicieux et très intélligent , mon père me disait toujours <span class="important">"Tu comprends vite fiston , mais faut t'expliquer longtemps"</span>. Je suis allé au collège de <span class="important">Francacatastrophe </span>, c'était
+                    un petit collège de 300 collégiens et autour il n'y avait que des champs, l'on pouvait percevoir les paysans du coins s'occuper de leurs vaches! <br>
+                    J'ai vraiment aimé mes <span class="important">5</span> années de collège car j'ai toujours été bien entouré de copains. <br>
+                    D'ailleurs c'est fou le changement de la petite école au collège, on apprends plein de nouvelles matières, ma préférée était <span class="important">l'allemand</span> mais on m'a interdit d'allemand, car quand je parlais je postillonnais sur mes camarades.</p>
+            </div>
+
+            <div class="contenu_main">
+                <h2>Lycée</h2>
+                <p> Alors là par où commencer ? <span class="souligne"> Mon premier jour </span> au lycée fût <span class="important">magnifique </span>, après avoir passé quelques mois loin de ma chérie <span class="important">"Élie Coptère" </span>( mais elle ne savait pas que je l'aimais au collège) je l'ai enfin revue, elle a fichtrement bien changée. <br>
+                    je savais pas que des <span class="souligne">nénés</span> pouvaient pousser si vite (c'est peut être comme les légumes, mon père mets des pesticides il dis que sa repousse les bactéries et que ça donne un meilleur goût.)
+                    C'est au lycée que j'ai rencontré mon meilleur ami, <span class="important">"Clint Horris"</span> il a grandi aux USA mais ses parents sont venus habiter en France, dès que je lui ai parlé on a tout de suite accroché ensemble, il est juste plus agé que moi car il a <span class="important">31 ans</span>, tout le monde
+                    se moquait de lui à cause de son âge mais les gens ne savent pas pourquoi il est au collège, c'est parce qu'il veut connaitre tous ses cours par coeur avant de partir du lycée, moi je trouve que c'est un malin.</p>
+            </div>
+
+            <div class="contenu_main">
+                <h2>Vie professionnelle</h2>
+                <p> Je suis sorti <span class="important">plus tôt </span>que mon ami Clint Horris car je n'ai pas la patience d'apprendre tous mes cours, j'aime charbonner et bouger mes doigts. A ma sortie de lycée j'avais <span class="important">22 ans </span>(et encore toutes mes dents ) je dis ça car Clint Horris lui il les perds
+                    il me dit que c'est à cause du stress des cours mais j'ai un doute je pense plutôt  que c'est parce qu'il se brosse les dents avec des cigarettes qu'il a fumé. En tout cas voilà dès ma sortie de lycée je suis allé travailler chez <span class="important">Roland Culé </span>, c'est un agriculteur
+                    qui vit a côté de la maison, il fait pousser des plantes médicinales qui se vendent <span class="important">très chères</span>, il a beaucoup de clients et moi j'ai beaucoup à apprendre de lui. Après quelques années passées à ses côtés je me dois de changer de métier car il y a des jours
+                    je crois que je ne supporte plus ce que Roland Culé fait, il brûle les mauvaises herbes mais moi après j'ai souvent envie de dormir. D'après mes parents c'est parce que le métier ne m'intéresse plus assez. C'est dommage je l'aimais bien mon patron et ce qu'il
+                    fallait faire. Je pense que je vais me diriger vers une nouvelle voie, j'aime mon village et je pense que je vais me lancer dans la propreté du village, je commencerai par faire les <span class="important">trottoirs</span>, comme ça je dirais à toutes les personnes que je croiserai d'arrêter
+                    de jeter leurs chewing gum avec le goût bizarre, j'en ai une fois ramassé un pour le goûter c'était pas bon , il avait un goût très âpre et le jus très salé. En plus ce sera peut être l'occasion de faire des rencontres car dans mon village
+                    il y a beaucoup de personnes qui <span class="important"> attendent </span> près du trottoir.</p>
+            </div>
+
+        </main>
+    </div>
+
+</div>
+
+
+<footer> <!-- Le footer commun à toutes les pages -->
+    <p>
+        Eric Hochet <br />
+        Telephone : 36.15 <br />
+        E-mail :
+        <a href="mailto:eric.hochet@gmail.com">eric.hochet@gmail.com</a>
+        <br><br>
+        <span id="disclaimer">Ce site est en contruction dans le cadre d'un projet pour la Wild Code School</span>
+    </p>
+
+</footer>
+
+</body>
+</html>
